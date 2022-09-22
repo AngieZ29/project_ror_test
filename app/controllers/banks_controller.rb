b0d134e89fddbf2777c562a337cb43d19fe6b191
Angela Zaldua <angelazaldua29@gmail.com>
2022-09-21 21:19:44 -0500

    spec create and view

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -2,4 +2,22 @@ class BanksController < ApplicationController
   def index
     @banks = Bank.all
   end
+
+  def new
+    @bank = Bank.new
+  end
+
+  def create
+    if (@bank = Bank.create(val_params))
+      redirect_to banks_path
+    else
+      render :create
+    end
+  end
+
+  private
+
+  def val_params
+    params.require(:bank).permit(:name)
+  end
 end
--- spec/controller/banks_controller_spec.rb
+++ spec/controller/banks_controller_spec.rb
@@ -21,6 +21,31 @@ RSpec.describe BanksController, type: :controller do
         get :index
         expect(response).to render_template('index')
       end
+
+      it 'responds successfully' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe 'POST#create' do
+    context 'when the name field is correct' do
+      req_bank = { bank: { name: Faker::Bank.name } }
+
+      it 'should return name bank' do
+        post :create, params: req_bank
+        expect(assigns(:bank).name).to eq(req_bank[:bank][:name])
+      end
+    end
+
+    context 'when there is not data' do
+      req_bank = { bank: { name: nil } }
+
+      it 'should return nil' do
+        post :create, params: req_bank
+        expect(assigns[:name]).to eq(nil)
+      end
     end
   end
 end
